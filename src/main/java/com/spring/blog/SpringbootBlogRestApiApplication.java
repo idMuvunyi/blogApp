dc2fae2b5f2875ac7fc5247ade35cc5bbc2a17ff
Muvunyi Idrissa <muvunyiiddy@gmail.com>
2023-06-22 11:58:18 +0200

    configure evironment profiles and deploy blog app to AWS Elastic Beanstalk

--- src/main/java/com/spring/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/spring/blog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,15 @@
 package com.spring.blog;
 
+import com.spring.blog.model.Role;
+import com.spring.blog.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -31,7 +35,7 @@ import org.springframework.context.annotation.Bean;
 				url = "https://github.com/idMuvunyi/blogApp"
 		)
 )
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 	@Bean
      public ModelMapper modelMapper(){
 		 return new ModelMapper();
@@ -40,4 +44,18 @@ public class SpringbootBlogRestApiApplication {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		// Role admin set
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		// Role user set
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
